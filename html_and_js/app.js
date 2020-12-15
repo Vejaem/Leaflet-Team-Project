aae5743114a67986ee6f0889afb971eb46026952
Vejae McLean <vejaem@yahoo.com>
2020-12-14 22:50:15 -0500

    Continued work

--- html_and_js/app.js
+++ html_and_js/app.js
@@ -1,3 +1,40 @@
-d3.csv("cbb.csv").then(function(data) {
+function init() {  
+  d3.csv("cbb.csv", function(d) {
+    return {
+      team : d.TEAM,
+      wins : +d.W,
+      year : +d.YEAR
+    };
+  }).then(function(data) {
     console.log(data);
-  });
+
+    var wins19 = data.filter(data => data.year == 2019).sort((a, b) => b.wins-a.wins);
+    console.log(wins19);
+
+    var wins18 = data.filter(data => data.year == 2018).sort((a, b) => b.wins-a.wins);
+
+    var wins17 = data.filter(data => data.year == 2017).sort((a, b) => b.wins-a.wins);
+
+    var wins16 = data.filter(data => data.year == 2016).sort((a, b) => b.wins-a.wins);
+
+    var wins15 = data.filter(data => data.year == 2015).sort((a, b) => b.wins-a.wins);
+    console.log(wins15);
+    
+    var numWins = wins15.wins
+    var teamWin = wins15.team
+
+    var barChart = [{
+      y: numWins.slice(0,5),
+      x: teamWin.slice(0,5)
+    }];
+
+    var barLayout = [{
+      title: "Top 5 Wins"
+    }];
+
+    Plotly.newPlot("bar", barChart, barLayout);
+  })
+}
+
+
+init();
