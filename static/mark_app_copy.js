29851c507c03b5df28dedee27cab1040269390a5
Brendan Cartin <brendan.cartin@gmail.com>
2020-12-21 15:23:26 -0500

    here

---
+++ static/mark_app_copy.js
@@ -0,0 +1,177 @@
+console.clear();
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Define SVG area dimensions
+var svgWidth = 1000;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var margin = {
+    top: 100,
+    right: 100,
+    bottom: 100,
+    left: 30
+};
+
+// width and height when taking margins into consideration
+var innerWidth = svgWidth - margin.left - margin.right;
+var innerHeight = svgHeight - margin.top - margin.bottom;
+
+//
+var svg = d3
+    .select("#myPlot")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+var chartGroup = svg.append("g");
+
+function init() {
+    // define default query
+    defaultTeam = "North Carolina";
+
+    buildCharts(defaultTeam);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// a function to make the plot 
+function buildCharts(queryTeam) {
+    d3.csv("../cbb.csv").then(function (data) {
+        var currentQuery = data.filter(function (team) {
+            return team.TEAM == queryTeam;
+        })
+        // First, sort the years b/c data in each object is not necessarily cronological
+        currentQuery.sort((a, b) => a.YEAR - b.YEAR);
+
+        console.log(currentQuery)
+
+        // map desired plotting data to variables so that you don't have to update all below: 
+        var xValue = currentQuery => +currentQuery.YEAR;
+        var yValue = currentQuery => +currentQuery.W;
+
+        // scale X vals
+        var xScale = d3.scaleBand()
+            .domain(currentQuery.map(d => xValue(d)))
+            // .domain(theXvalues)
+
+            // the range needs to take into consideration the translation and margins:
+            .range([0, innerWidth - margin.left - margin.right])
+            .padding(0.1);
+
+        // scale Y vals
+        var yScale = d3.scaleLinear()
+            .domain([0, d3.max(currentQuery, yValue)])
+            .range([innerHeight, 0]);
+
+        var xAxis = d3.axisBottom(xScale);
+        var yAxis = d3.axisLeft(yScale);
+
+        // make g(roup) element to translate plot
+        chartGroup.append('g')
+            // translate the extent of the margins
+            .attr('transform', `translate(${margin.left + margin.right}, ${margin.top})`);
+
+        xAxis(chartGroup.append('g')
+            .attr("class", "x axis")
+            .attr('transform', `translate(0, ${innerHeight})`)
+            .style("font", "30px times"));
+
+            chartGroup.append("text")
+            .attr("class", 'x label')
+            .attr("text-anchor", "end")
+            .attr("x", (innerWidth - margin.left - margin.right + 50) / 2)
+            .attr("y", innerHeight + margin.top - 20)
+            .text("Year")
+            .style("font", "40px times");
+
+            chartGroup.append("text")
+            .attr("class", 'y label')
+            .attr("text-anchor", "end")
+            .attr("transform", "translate(-50, 200)rotate(-90)")
+            .text("Wins")
+            .style("font", "40px times");
+
+        yAxis(.chartGroup.append('g')
+            .style("font", "30px times"));
+
+        // make bars
+        // originally I appended to svg.select all. Later, I appended to "g.selectAll" so that chart could be translated
+        chartGroup.selectAll('circle').data(currentQuery)
+            .enter().append('circle')
+
+            .attr('cx', d => xScale(xValue(d)))
+            .attr('cy', d => yScale(yValue(d)))
+            .attr("transform", "translate(70,0)") 
+            .attr('r', 20)
+            // .attr('dx', 10000) //'transform, translate(1000, 0)')
+            // .attr("transform", "translate(1000,1000)")
+            .attr('height', d => innerHeight - yScale(yValue(d)))
+            .attr('width', xScale.bandwidth())
+            .attr('fill', 'red')
+            .on("mouseover", function () {
+                d3.select(this)
+                    .attr("fill", "black")
+            })
+            .on("click", function (d) {
+                toolTip.show(d, this);
+            })
+            .on("mouseout", function (d) {
+                toolTip.hide(d);
+                d3.select(this)
+                    .attr("fill", "red")
+            });
+    });
+
+};
+
+////////////////////////////////////////////////////////////////////////////
+function makeDrops() {
+    d3.csv("../cbb.csv").then(function (data) {
+
+        queryItem2 = "2016";
+        var currentQuery2 = data.filter(function (team) {
+            return team.YEAR == queryItem2;
+        })
+
+        var selector = d3.select("#drop")
+            .append("select")
+            .attr("id", "dropdown")
+            .on("change", function (d) {
+                selection = document.getElementById("dropdown");
+                d3.selectAll("circle").remove()
+                 d3.selectAll("yAxis").remove()
+                  d3.selectAll(".tick").remove()
+                drillLevel = 1;
+                buildCharts(selection.value)
+            });
+
+        selector.selectAll("option")
+            .data(currentQuery2)
+            .enter().append("option")
+            .attr("value", function (d) {
+                return d.TEAM;
+            })
+            .text(function (d) {
+                return d.TEAM;
+            })
+    });
+};
+
+init();
+makeDrops();
+
+var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([150, 0])
+    .html(function (d) {
+        return (`<p style="color:black"; align = "center"; style="font-size:10px">
+            ${(d.CONF)}<br>
+            Games Played: ${d.G}<br>
+            Wins: ${d.W}<br>
+            FG%: ${d.EFG_O}<br>
+            Tourney: ${d.POSTSEASON}<br>
+            </p>`);
+    });
+
+chartGroup.call(toolTip);
